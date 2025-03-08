ed0254653e12166cb2d7b65b824b53610ef618ed
hassan <hassanibrahimndahiro@gmail.com>
2025-03-08 23:40:26 +0300

    configured app to use Postgre

--- app/config.py
+++ app/config.py
@@ -5,11 +5,10 @@ load_dotenv()  # Load environment variables from .env file
 
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Get absolute path of the project
-    INSTANCE_DIR = os.path.join(BASE_DIR,'..', 'instance')  # Path to the instance directory
-    DATABASE_URL = os.getenv('DATABASE_URL',"postgresql://money_mate_db_user:OQVveuBE79DKAOz2KEN0szI5zf5Lh6IX@dpg-cv69re2j1k6c73e3no1g-a/money_mate_db")
+    DATABASE_URL = os.getenv('DATABASE_URL')
 
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     DEBUG = os.getenv('FLASK_DEBUG', 'False').lower() in ('true', '1')
     # PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
