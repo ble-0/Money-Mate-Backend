767a602c5fa237c9e0cd37709018e26d3c972a5c
hassan <hassanibrahimndahiro@gmail.com>
2025-03-08 23:17:28 +0300

    changed the data base to postgre

--- app/config.py
+++ app/config.py
@@ -6,9 +6,9 @@ load_dotenv()  # Load environment variables from .env file
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Get absolute path of the project
     INSTANCE_DIR = os.path.join(BASE_DIR,'..', 'instance')  # Path to the instance directory
-    DATABASE_URL = os.getenv('DATABASE_URL', f"sqlite:///{os.path.join(INSTANCE_DIR, 'money_mate.db')}")
+    DATABASE_URL = os.getenv('DATABASE_URL',"postgresql://money_mate_db_user:OQVveuBE79DKAOz2KEN0szI5zf5Lh6IX@dpg-cv69re2j1k6c73e3no1g-a/money_mate_db")
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///../instance/money_mate.db"
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
     DEBUG = os.getenv('FLASK_DEBUG', 'False').lower() in ('true', '1')
